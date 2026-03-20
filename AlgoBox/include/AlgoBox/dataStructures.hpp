0231c2249185ccfb3bafb74d890c9e6ed96e5c87
fearrr1337 <fearren01@gmail.com>
2026-03-20 21:54:15 +0300

    Add queue

--- AlgoBox/include/AlgoBox/dataStructures.hpp
+++ AlgoBox/include/AlgoBox/dataStructures.hpp
@@ -49,4 +49,90 @@ namespace ab {
 		}
 	};
 
+	// =======================================================================================
+
+
+	// Queue
+	template<typename T>
+	class queue {
+	private:
+		T* data; // arr
+		size_t capacity; // size memory (max elements)
+		size_t head; // first element (index)
+		size_t tail; // last element (index)
+		size_t count; // count elements in queue
+
+		// Resize array
+		void resize() {
+			size_t new_capacity = capacity * 2;
+			T* new_data = new T[new_capacity];
+			for (size_t i = 0; i < count; ++i) {
+				new_data[i] = data[(head + 1) % capacity];
+			}
+			delete[] data;
+			data = new_data;
+			capacity = new_capacity;
+			head = 0;
+			tail = count;
+		}
+
+	public:
+		queue(size_t cap = 8) : capacity(cap), head(0), tail(0), count(0) { data = new T[capacity]; }
+
+		~queue() { delete[] data; }
+
+		// Add element
+		void push(const T& value) {
+			if (count == capacity) {
+				resize();
+			}
+
+			data[tail] = value;
+			tail = (tail + 1) % capacity;
+			++count;
+		}
+
+		// Del element
+		void pop() {
+			if (empty()) {
+				throw std::out_of_range("queue is empty");
+			}
+			head = (head + 1) % capacity;
+			--count;
+		}
+
+		// First element
+		T& front() {
+			if (empty()) {
+				throw std::out_of_range("queue is empty");
+			}
+			return data[head];
+		}
+
+		const T& front() const {
+            if (empty())
+                throw std::out_of_range("queue is empty");
+
+            return data[head];
+        }
+
+		// Last element
+		T& back() {
+			if (empty()) {
+				throw std::out_of_range("queue is empty");
+			}
+
+			return data[(tail + capacity - 1) % capacity];
+		}
+
+		bool empty() const {
+			return count == 0;
+		}
+
+		size_t size() const {
+			return count;
+		}
+	};
+
+
 }
